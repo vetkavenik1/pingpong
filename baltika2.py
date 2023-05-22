a73cbefbc606f4028f427d2feaff3b5c58c734f5
vetkavenik1 <134287312+vetkavenik1@users.noreply.github.com>
2023-05-22 17:44:49 +0500

    Update baltika2.py
    это вторая версия кода, меня заставили это делать :)
    тут есть классы!

--- baltika2.py
+++ baltika2.py
@@ -8,6 +8,50 @@ clock = time.Clock()
 FPS = 60
 
 
+
+
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        #Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+ 
+        #каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+        #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+
+class Player(GameSprite):
+    #метод для управления спрайтом стрелками клавиатуры
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+
+
+
 game = True
 while game:
     for e in event.get():
